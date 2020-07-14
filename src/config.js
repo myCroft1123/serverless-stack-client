0a8684680e4627cd595f0927bc051f406ca232d1
myCroft1123 <sean.odell813@gmail.com>
2020-07-14 16:26:17 -0700

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-uploads-sean"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://43b8svpcx4.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_R9l6omMUA",
+      APP_CLIENT_ID: "7g9h9rha6lhkhfcdm3r9kpfapl",
+      IDENTITY_POOL_ID: "us-east-1:98c13cea-7d46-4d0b-97d2-f240e89ed73b"
+    }
+  };
